0915bdc333193f3fc4fa547c706b3e8870530bc1
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-02-20 17:54:50 -0300

    feat: add react-query and request data in home screen

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,41 @@ import {
   TableHeader,
   TableRow,
 } from "./components/ui/table";
+import { useQuery } from "@tanstack/react-query";
+
+export type TagResponse = {
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
+};
+
+type Tag = {
+  title: string;
+  amountOfVideos: number;
+  id: string;
+};
 
 export function App() {
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ["get-tags"],
+    queryFn: async () => {
+      const response = await fetch(
+        "http://localhost:3333/tags?_page=1&_per_page=10",
+      );
+      const data = await response.json();
+
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -53,19 +86,19 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((_, index) => {
+            {tagsResponse?.data.map((tag) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className="flex flex-col">
-                      <span className="font-medium">React</span>
-                      <span className="text-xs  text-zinc-500">
-                        A08BCACBA-CBA89-298CAB9-2138-FA42
-                      </span>
+                      <span className="font-medium">{tag.title}</span>
+                      <span className="text-xs  text-zinc-500">{tag.id}</span>
                     </div>
                   </TableCell>
-                  <TableCell className="text-zinc-300">13 vídeo(s)</TableCell>
+                  <TableCell className="text-zinc-300">
+                    {tag.amountOfVideos} vídeo(s)
+                  </TableCell>
                   <TableCell>
                     <Button size="icon">
                       <MoreHorizontal className="size-4" />
