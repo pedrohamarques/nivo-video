fdb119d10cfcd1b1beeed56a6b7f60307347573e
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-02-20 16:53:05 -0300

    feat: add table and others components to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,82 @@
-import { Plus } from 'lucide-react'
+import { FileDown, MoreHorizontal, Plus, Search } from "lucide-react";
+import { Header } from "./components/header";
+import { Tabs } from "./components/tabs";
+import { Button } from "./components/ui/button";
+import { Control, Input } from "./components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
 
 export function App() {
   return (
     <div className="py-10 space-y-8">
-    <div>
-      header
-      tabs
-    </div>
-    <main className="max-w-6xl mx-auto space-y-5">
-      <div className='flex items-center gap-3'>
-      <h1 className="text-xl font-bold">Tags</h1>
-      <button className='inline-flex items-center gap-1.5 bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1'>
-        <Plus className='size-3'/>
-        Create new
-      </button>
+      <div>
+        <Header />
+        <Tabs />
       </div>
-    </main>
-    </div>
-  )
-}
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <Button
+            variant="primary"
+            className="inline-flex items-center gap-1.5 bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1"
+          >
+            <Plus className="size-3" />
+            Create new
+          </Button>
+        </div>
 
+        <div className="flex items-center justify-between">
+          <Input variant="filter">
+            <Search className="size-3" />
+            <Control placeholder="Search tags..." />
+          </Input>
+
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
+        </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 10 }).map((_, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col">
+                      <span className="font-medium">React</span>
+                      <span className="text-xs  text-zinc-500">
+                        A08BCACBA-CBA89-298CAB9-2138-FA42
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-300">13 vídeo(s)</TableCell>
+                  <TableCell>
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </main>
+    </div>
+  );
+}
